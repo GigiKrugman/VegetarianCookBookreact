0b71561d1f23712ea1bd09da34f11a2391ce7569
Marcello Mulas <marcellomulas93@gmail.com>
2023-04-03 12:35:28 +0200

    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./assets/CSS/Popular.css";
+import "./assets/CSS/Searched.css";
+import "./assets/CSS/Navbar.css";
+import "./assets/CSS/Detail.css";
+import "./assets/CSS/SearchBar.css";
 //import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
