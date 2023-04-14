179a6037af163a48527d41606902d2a57c319328
Marcello Mulas <marcellomulas93@gmail.com>
2023-04-14 10:03:04 +0200

    Some final changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-// import Pages from "./components/Pages";
-// import { BrowserRouter } from "react-router-dom";
-//import "./index.css";
 import "./index.css";
-//import { BrowserRouter } from "react-router-dom";
-// import SearchBar from "./assets/CSS/SearchBar.css";
-// import Navbar from "./assets/CSS/Navbar.css";
 import Pages from "./pages/Pages";
 import { BrowserRouter } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
